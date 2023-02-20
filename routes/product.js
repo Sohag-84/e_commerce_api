03fb5ac4cee78d5c20010a569cf91b114749c1cf
sohag-84 <injamulhaqsohag19977@gmail.com>
2023-02-20 15:47:22 +0600

    deal of the day product api

--- routes/product.js
+++ routes/product.js
@@ -5,13 +5,13 @@ const Product = require("../models/product");
 
 //get category product
 productRouter.get("/api/products", auth, async (req, res) => {
-    try {
-        console.log(req.query.category);
-        const products = await Product.find({ category: req.query.category });
-        res.json(products);
-    } catch (e) {
-        res.status(500).json({ error: e.message });
-    }
+  try {
+    console.log(req.query.category);
+    const products = await Product.find({ category: req.query.category });
+    res.json(products);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
 });
 
 //create a get request to search product and get them
@@ -48,6 +48,31 @@ productRouter.post("/api/rate-product", auth, async (req, res) => {
     product.ratings.push(ratingSchema);
     product = await product.save();
     res.json(product);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
+//deal of the day based on product ratings
+productRouter.get("/api/deal-of-day", auth, async (req, res) => {
+  try {
+    let products = await Product.find({});
+
+    products = products.sort((a, b) => {
+      let aSum = 0;
+      let bSum = 0;
+
+      for (let i = 0; i < a.ratings.length; i++) {
+        aSum += a.ratings[i].rating;
+      }
+
+      for (let i = 0; i < b.ratings.length; i++) {
+        bSum += b.ratings[i].rating;
+      }
+      return aSum < bSum ? 1 : -1;
+    });
+
+    res.json(products[0]);
   } catch (e) {
     res.status(500).json({ error: e.message });
   }
